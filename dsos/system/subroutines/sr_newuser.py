479d2d586451247e4f4cf11f3aa5555cc1db2438
Kedev <80370389+cs-AI-dev@users.noreply.github.com>
2022-02-14 11:57:43 -0600

    Update sr_newuser.py

--- dsos/system/subroutines/sr_newuser.py
+++ dsos/system/subroutines/sr_newuser.py
@@ -1,1 +1,7 @@
-
+def executeSubroutine(username, password, permissions="Default"):
+	fdir = __file__.split("/subroutines/sr_newuser.py")[0] + "/users/" + username + ".user", "x"
+	with open(fdir, "x") as f:
+		pass
+	with open(fdir, "w") as f:
+		encryptedPass = executeSubroutine("nkc", password)
+		f.write(f"username:{username};password:{encryptedPass};permissions:{permissions}")
